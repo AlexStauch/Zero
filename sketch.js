0b3d99c4bf42e85323417fb2e7a7ab08bf0f6ccf
AlexStauch <alexnstauch@gmail.com>
2020-01-09 11:32:16 -0800

    second commit

--- sketch.js
+++ sketch.js
@@ -3,11 +3,14 @@ var frames = 0;
 function setup() {
   createCanvas(1262, 600);
   background(108, 111, 101);
+  noCursor();
 }
 
 function draw() {
- 	stroke(108, 111, 101);
- 	strokeWeight(15);
+ 	frames++
+
+ 	stroke((frames%108), (frames%111), (frames%101));
+ 	strokeWeight((frames*2)%20);
  	fill(0);
 	ellipse(mouseX, mouseY, 200, 200);
 }
