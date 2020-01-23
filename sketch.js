94033589527c50f41274dfb06e37bb7fef23208b
AlexStauch <alexnstauch@gmail.com>
2020-01-22 22:09:58 -0800

    third commit

--- sketch.js
+++ sketch.js
@@ -1,16 +1,34 @@
-var frames = 0;
+var frames;
 
-function setup() {
-  createCanvas(1262, 600);
-  background(108, 111, 101);
-  noCursor();
+function setup(){
+	var cnv = createCanvas(windowWidth, windowHeight);
+	cnv.style('display', 'block');
+	background(0);
 }
 
-function draw() {
- 	frames++
+function windowResized(){
+	createCanvas(windowWidth, windowHeight);
+}
 
- 	stroke((frames%108), (frames%111), (frames%101));
- 	strokeWeight((frames*2)%20);
- 	fill(0);
-	ellipse(mouseX, mouseY, 200, 200);
+function draw(){
+ frames++;
+ var frames = frames%100
+
+  for(var i = 0; i < 100; i++){
+	stroke(.85 * i, 2.45*i, 2.15* i);
+	strokeCap(PROJECT);
+	strokeWeight(5);
+	line(width/2.05, height/2.5, 20*i, 20*i);
+	line(width/1.95, height/1.5, width - (20*i), height - (20*i));
+	curve(0 , 0, 0 - (i*20), i*20, i*20, (i*20), width, height);
+	curve(width, height, width - (20*i), height - (20*i), (width- (20*i)) * 2, (height - (20*i)) * 2, width/1.95, 0);
+	noFill();
+  }
+
+  applyMatrix();
+  fill(215, 240, 215);
+  triangle(width/3.7, 0, width/2.62, height/2.5, width/1.95, height/1.5);
+  triangle(width/2.05, height/2.5, width/1.61, height/1.5, width/1.4, height);
+  resetMatrix();
+  
 }
